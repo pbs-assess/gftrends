eed7ba34601a838d4aa3e0190f3389c2954955b3
Sean Anderson <sean@seananderson.ca>
2021-01-06 17:38:11 -0800

    Make basic ts plot

---
+++ analysis/stitch-data.R
@@ -0,0 +1,19 @@
+f <- list.files("data-raw", pattern = ".rds", full.names = TRUE)
+
+d <- purrr::map_dfr(f, readRDS)
+
+library(tidyverse)
+
+d %>% mutate(stock = paste(species, region)) %>%
+  ggplot(aes(
+    x = year,
+    y = exp(log_blrp),
+    ymin = exp(log_blrp - 2 * sd_log_blrp),
+    ymax = exp(log_blrp + 2 * sd_log_blrp))) +
+  geom_ribbon(colour = NA, fill = "grey50", alpha = 0.4) +
+  geom_line() +
+  facet_wrap(~stock) +
+  scale_y_log10() +
+  geom_hline(yintercept = 1, lty = 2) +
+  coord_cartesian(xlim = c(1960, 2020)) +
+  ylab("B/LRP") + theme(axis.title.x = element_blank())
