6cf65a44653ab410115ecf10dc77092826e893eb
Sean Anderson <sean@seananderson.ca>
2021-01-14 17:13:52 -0800

    Add latent states to main plot
    Former-commit-id: 2ffcc6ca965e9c79d6fb2f474ec174790bef6c6a [formerly ffdc06bfaf6ac8c50fa39dcd13c0ae95603da249]
    Former-commit-id: 3d472905aad86c589c08bc732eb20b44ef1f7e7e
    Former-commit-id: 14fd1384b069af0b292701fd8ec02c031281c38d

--- analysis/03-plot-models.R
+++ analysis/03-plot-models.R
@@ -7,12 +7,25 @@ m <- readRDS("data-generated/b-ratio-fits.rds")
 d <- readRDS("data-generated/b-ratio-fits-data.rds")
 dat <- readRDS("data-generated/b-status-dat.rds")
 
-plot_x_t <- function(x_t, col_log_mean, col_q0.05, col_q0.95, ylab = "", ylim = c(0, 10)) {
+plot_x_t <- function(x_t, .y_true, .fitted_dat, col_log_mean, col_q0.05, col_q0.95, ylab = "", ylim = c(0, 10)) {
+
   last_dat <- dat %>%
     group_by(stock) %>%
     mutate(last_status = exp({{col_log_mean}})[n()]) %>%
     ungroup() %>%
     mutate(stock = gsub("_", " ", stock))
+
+  stock_ids <- distinct(select(.fitted_dat, stock)) %>%
+    arrange(stock) %>%
+    mutate(j = seq_len(n()))
+  year_ids <- distinct(select(.fitted_dat, year)) %>%
+    filter(year >= 1950, year <= 2020) %>%
+    arrange(year) %>%
+    mutate(.t = seq(1, n()))
+  .y_true <- left_join(.y_true, stock_ids) %>% left_join(year_ids)
+  .y_true <- mutate(.y_true, stock = gsub("_", " ", stock))
+
+  .y_true <- left_join(.y_true, distinct(select(last_dat, stock, last_status)))
 
   x_t %>%
     mutate(.value = exp(.value)) %>%
@@ -31,7 +44,9 @@ plot_x_t <- function(x_t, col_log_mean, col_q0.05, col_q0.95, ylab = "", ylim = 
     geom_ribbon(aes(ymin = lwr2, ymax = upr2), fill = "grey70", alpha = 0.6) +
     geom_ribbon(aes(ymin = lwr, ymax = upr), fill = "grey55", alpha = 0.6) +
     geom_ribbon(aes(ymin = lwr1, ymax = upr1), fill = "grey40", alpha = 0.6) +
-    geom_line(lwd = 0.7) +
+    geom_line(mapping = aes(x = year, y = exp(.value), group = .draw, colour = last_status),
+      alpha = 0.3, lwd = 0.15, data = .y_true, inherit.aes = FALSE) +
+    geom_line(lwd = 0.7, alpha = 0.75) +
     geom_ribbon(aes(
       x = year,
       y = exp({{col_log_mean}}),
@@ -63,41 +78,40 @@ plot_x_t <- function(x_t, col_log_mean, col_q0.05, col_q0.95, ylab = "", ylim = 
 }
 
 x_t <- lapply(m, function(.x) tidybayes::gather_draws(.x, x[.t]))
-g <- plot_x_t(x_t[["blrp"]], log_blrp, q0.05_blrp, q0.95_blrp,
+y_true <- lapply(m, function(.x) {
+  result <- tidybayes::gather_draws(.x, y_true[.t, j], n = 30L)
+  filter(result, .value != 0) # fake
+})
+
+g <- plot_x_t(x_t[["blrp"]], y_true[["blrp"]], d[["blrp"]]$filtered_dat,
+  log_blrp, q0.05_blrp, q0.95_blrp,
   ylab = expression(B/LRP), ylim = c(0, 10))
-ggsave("figs/blrp-x-t.pdf", width = 10, height = 7)
+ggsave("figs/blrp-x-t.pdf", width = 10, height = 7.5)
+ggsave("figs/blrp-x-t.png", width = 10, height = 7.5)
 
-g <- plot_x_t(x_t[["busr"]], log_busr, q0.05_busr, q0.95_busr,
+g <- plot_x_t(x_t[["busr"]], y_true[["busr"]], d[["busr"]]$filtered_dat,
+  log_busr, q0.05_busr, q0.95_busr,
   ylab = expression(B/USR), ylim = c(0, 5))
-ggsave("figs/busr-x-t.pdf", width = 10, height = 7)
+ggsave("figs/busr-x-t.pdf", width = 10, height = 7.5)
+ggsave("figs/busr-x-t.png", width = 10, height = 7.5)
 
-g <- plot_x_t(x_t[["bbmsy"]], log_bbmsy, q0.05_bmsy, q0.95_bmsy,
+g <- plot_x_t(x_t[["bbmsy"]], y_true[["bbmsy"]], d[["bbmsy"]]$filtered_dat,
+  log_bbmsy, q0.05_bmsy, q0.95_bmsy,
   ylab = expression(B/B[MSY]), ylim = c(0, 3.5))
-ggsave("figs/bbmsy-x-t.pdf", width = 10, height = 7)
+ggsave("figs/bbmsy-x-t.pdf", width = 10, height = 7.5)
+ggsave("figs/bbmsy-x-t.png", width = 10, height = 7.5)
 
-y_true <- tidybayes::gather_draws(m[[1]], y_true[.t, j], n = 30)
-y_true <- filter(y_true, .value != 0) # fake
-
-stock_ids <- distinct(select(d[[1]]$filtered_dat, stock)) %>%
-  arrange(stock) %>%
-  mutate(j = seq_len(n()))
-year_ids <- distinct(select(d[[1]]$filtered_dat, year)) %>%
-  filter(year >= 1950, year <= 2020) %>%
-  arrange(year) %>%
-  mutate(.t = seq_len(n()))
-y_true <- left_join(y_true, stock_ids) %>% left_join(year_ids)
-
-g <- ggplot(y_true, aes(year, exp(.value), group = .draw)) +
-  geom_line(alpha = 0.3, lwd = 0.2) +
-  facet_wrap(~stock) +
-  geom_line(aes(x = year, y = exp(log_blrp)),
-    data = filter(dat, year >= 1950), colour = "red",
-    inherit.aes = FALSE
-  ) +
-  geom_ribbon(aes(x = year, y = exp(log_blrp), ymin = q0.05_blrp, ymax = q0.95_blrp),
-    data = filter(dat, year >= 1950), colour = NA, fill = "red", alpha = 0.2,
-    inherit.aes = FALSE
-  ) +
-  ggsidekick::theme_sleek() +
-  coord_cartesian(xlim = c(1950, 2020), ylim = c(0, 10))
-ggsave("figs/blrp-stock-latent.png", width = 10, height = 7)
+# g <- ggplot(y_true, aes(year, exp(.value), group = .draw)) +
+#   geom_line(alpha = 0.3, lwd = 0.2) +
+#   facet_wrap(~stock) +
+#   geom_line(aes(x = year, y = exp(log_blrp)),
+#     data = filter(dat, year >= 1950, year <= 2020), colour = "red",
+#     inherit.aes = FALSE
+#   ) +
+#   geom_ribbon(aes(x = year, y = exp(log_blrp), ymin = q0.05_blrp, ymax = q0.95_blrp),
+#     data = filter(dat, year >= 1950, year <= 2020), colour = NA, fill = "red", alpha = 0.2,
+#     inherit.aes = FALSE
+#   ) +
+#   ggsidekick::theme_sleek() +
+#   coord_cartesian(xlim = c(1950, 2020), ylim = c(0, 10))
+# ggsave("figs/blrp-stock-latent.png", width = 10, height = 7)
