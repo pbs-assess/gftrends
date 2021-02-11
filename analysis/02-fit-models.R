abd7589fcaaa1bd56635b8ec64fb8e28412d6790
Sean Anderson <sean@seananderson.ca>
2021-02-10 19:06:24 -0800

    Up iterations; cleanup
    Former-commit-id: 4c359d13871776ce558538733d246df7391b400c [formerly d77a85d547bc7ac1db93bcdbe99b19fa991b1d15]
    Former-commit-id: a286a264a2ca457fe0d7bb6318a677a6e1a6dd3e
    Former-commit-id: 2e7ac6dec20406726f098afa0cc82d2084e4d735

--- analysis/02-fit-models.R
+++ analysis/02-fit-models.R
@@ -23,7 +23,7 @@ m <- purrr::map(d, function(.d) {
   rstan::sampling(
     model,
     data = .d$stan_dat,
-    chains = 6, iter = 600,
+    chains = 6, iter = 1000,
     # init = initf,
     pars = pars
   )
@@ -37,39 +37,5 @@ purrr::walk(m, print, pars = pars[!pars %in% c("y_true", "x")])
 # lapply(m, rstan::stan_rhat)
 # lapply(m, rstan::stan_diag)
 
-# p <- purrr::map(m, rstan::extract)
-
 saveRDS(m, "data-generated/b-ratio-fits.rds")
 saveRDS(d, "data-generated/b-ratio-fits-data.rds")
-
-# eps <- p$eps
-# means <- apply(eps, c(2, 3), mean)
-# sds <- apply(eps, c(2, 3), sd)
-# matplot(means, type = "l")
-# matplot(sds, type = "l")
-#
-# miss <- p$y_m
-# apply(miss, 2, mean)
-# apply(miss, 2, sd)
-#
-# # shinystan::launch_shinystan(m)
-#
-# apply(p$alpha, 2, mean)
-# mean(apply(p$alpha, 2, mean))
-
-# pp <- apply(p$x, 2, function(x, ...) median(exp(x), ...), na.rm = TRUE)
-# plot(seq_along(pp), pp, lwd = 3, lty = 2, type = "l")
-
-# dd <- as.matrix(dat_wide[, -1])
-# dd[dd == 999] <- NA
-# matplot(exp(dd), type = "l", log = "y")
-
-# x <- apply(p$eps, c(2, 3), mean, na.rm = TRUE)
-# matplot(x, type = "l")
-
-# x <- apply(p$y_true, c(2, 3), mean, na.rm = TRUE)
-# matplot(exp(x), type = "l", log = "y")
-
-# hist(p$rho)
-# hist(p$sigma_eps)
-# hist(p$sigma_x)
