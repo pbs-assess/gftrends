5cff0af9f91f29abf5ea9a259e529a917fad4405
Sean Anderson <sean@seananderson.ca>
2022-03-02 14:45:11 -0800

    Simplify

--- analysis-survey/2-render-indices.R
+++ analysis-survey/2-render-indices.R
@@ -1,96 +1,4 @@
-# getwd()
 library(dplyr)
-
-dir.create(file.path(here::here("analysis-survey", "data")))
-dir.create(file.path(here::here("analysis-survey", "figs")))
-dir.create(file.path(here::here("analysis-survey", "indices")))
-
-dir.create(file.path(here::here("analysis-survey", "html")))
-
-
-list_regions <- c("Both odd year trawl surveys", "WCVI only")
-list_species <- c(
-  "Pacific Cod" # 3CD (WCVI), 5ABCD (Both odd year)
-)
-
-list_regions <- c("WCHG only", "QCS & WCVI") # "North (PMFC 5DE)", "South (PMFC 3CD5ABC)",
-list_species <- c(
-  "Redstripe Rockfish", # N (PMFC 5DE) and South (PMFC 3CD5ABC)
-  "Rougheye/Blackspotted Rockfish Complex" # N = HS (technically only north half) and WCHG, and S + QCS and WCVI
-)
-
-list_regions <- c("QCS only", "WCVI only", "WCHG only") # "North (PMFC 5DE)", "South 5 only (PMFC 5ABC)",
-list_species <- c(
-  "Pacific Ocean Perch"
-)
-
-list_regions <- c("QCS only", "HS only")
-list_species <- c(
-  "Southern Rock Sole" # 5AB (QCS),  5CD (HS) low density along boundary so close enough
-)
-
-list_regions <- c("HS & WCHG", "QCS & WCVI")
-list_species <- c(
-  "Walleye Pollock" # N = HS and WCHG, and S + QCS and WCVI
-)
-
-
-# initial coastwide trawl models
-list_regions <- c("Coast-wide trawl surveys")
-list_species <- c(
-  "North Pacific Spiny Dogfish",
-  "Arrowtooth Flounder",
-  "Bocaccio",
-  "Sablefish",
-  "Shortspine Thornyhead",
-  "Silvergray Rockfish",
-  "Widow Rockfish",
-  "Yellowmouth Rockfish",
-  "Yellowtail Rockfish"
-
-  # "Quillback Rockfish", # outside and WCVI_Inside?
-  # redo with specific subsets of surveys?
-  # "Pacific Ocean Perch", #3CD (WCVI), 5ABC, 5DE (Both odd year + HG)
-  # "Pacific Cod", #3CD (WCVI), 5ABCD (Both odd year)
-  # "Southern Rock Sole", # 5AB (QCS),  5CD (HS)
-  # redo with N-S split?
-  # "Walleye Pollock", # N and S?
-  # "Redstripe Rockfish", # N and S?
-  # "Rougheye/Blackspotted Rockfish Complex"# N and S?
-)
-
-# list_regions <- c("HBLL outside surveys")
-# list_species <- c(
-#   "North Pacific Spiny Dogfish",
-#   # "Arrowtooth Flounder",
-#   # "Bocaccio",
-#   "Sablefish",
-#   # "Shortspine Thornyhead",
-#   # "Silvergray Rockfish",
-#   # "Widow Rockfish",
-#   # "Yellowtail Rockfish",
-#   # "Yellowmouth Rockfish",
-#   ## ones with potentially awkward splits
-#   # "Walleye Pollock", # N and S?
-#   # "Pacific Cod", #3CD (WCVI), 5ABCD (Both odd year)
-#   # "Southern Rock Sole", # 5AB (QCS),  5CD (HS)
-#   # "Redstripe Rockfish", # N and S?
-#   # "Rougheye/Blackspotted Rockfish Complex",# N and S?
-#   # "Lingcod", #4B = inside
-#   "Quillback Rockfish", # outside and WCVI_Inside?
-#   "Yelloweye Rockfish" # 4B = inside and outside
-# )
-#
-# list_regions <- c("HBLL inside surveys")
-# list_species <- c(
-#   "North Pacific Spiny Dogfish",
-#   "Lingcod", #4B = inside
-#   "Quillback Rockfish", # outside and WCVI_Inside?
-#   "Yelloweye Rockfish" # 4B = inside
-#   # "Big Skate"
-# )
-
-# also need to come up with a list of other species of interest...
 
 list_regions <- c("Coast-wide trawl surveys")
 list_species <- c(
@@ -222,29 +130,31 @@ to_fit <- bind_rows(
   )
 )
 
-list_species <-
-  for (r_h in seq_along(list_regions)) {
-    for (spp_i in seq_along(list_species)) {
-      spp <- gsub(" ", "-", gsub("\\/", "-", tolower(list_species[spp_i])))
-      name <- "-RW-no-covs" # string describing model covariates
-      region_name <- list_regions[r_h]
-      try({
-        rmarkdown::render("analysis-survey/1-index-standardization.Rmd",
-          params = list(
-            species = list_species[spp_i],
-            region = list_regions[r_h],
-            delta_model = FALSE,
-            update_model = FALSE,
-            update_index = TRUE
-            # update_index = FALSE
-          ),
-          output_file = paste0(spp, name, "-", region_name, "-delta.html"),
-          envir = env
-        )
-      })
-    }
-  }
+# https://github.com/rstudio/rmarkdown/issues/1673
+render_separately <- function(...) callr::r(
+  function(...) rmarkdown::render(..., envir = globalenv()),
+  args = list(...), show = TRUE)
 
+fit_index <- function(region, species) {
+  spp <- gsub(" ", "-", gsub("\\/", "-", tolower(species)))
+  name <- "-RW-no-covs" # string describing model covariates
+  region_name <- region
+  try({
+    render_separately("analysis-survey/1-index-standardization.Rmd",
+      params = list(
+        species = species,
+        region = region,
+        delta_model = TRUE,
+        update_model = TRUE,
+        update_index = TRUE
+        # update_index = FALSE
+      ),
+      output_file = paste0(spp, name, "-", region_name, "-delta.html")
+    )
+  })
+}
+
+purrr::pwalk(to_fit[1,,drop=FALSE], fit_index)
 
 # # full list from 2 years ago
 # list_species <- c(
