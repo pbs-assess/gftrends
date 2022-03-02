0e87be2b524909b7792b14e505aeb60d7141bcf8
ecophilina <paenglis@lakeheadu.ca>
2022-03-01 21:56:38 -0800

    style

--- analysis-survey/3-plot-surv-indices.R
+++ analysis-survey/3-plot-surv-indices.R
@@ -6,7 +6,7 @@ source("analysis/stock_df2.R")
 ####################
 # Run first part only if you have all indices saved in separate files
 # and they have been updated
-mydir = paste0("analysis-survey/indices/")
+mydir <- paste0("analysis-survey/indices/")
 myfiles <- list.files(path = mydir, pattern = "*.rds", full.names = TRUE)
 myfiles
 all_indices <- do.call(bind_rows, lapply(myfiles, readRDS))
@@ -16,38 +16,54 @@ saveRDS(all_indices, file = paste0("analysis-survey/data/sopo-2021-indices.rds")
 
 all_indices <- readRDS(paste0("analysis-survey/data/sopo-2021-indices.rds")) %>%
   rename(gear = type)
-dat <- readRDS("data-generated/b-status-dat.rds") %>% filter(year >= 2000) %>%
-  rename(dfo_area = region) %>% select(-species) %>% group_by(stock) %>%
-  mutate(mean_blrp = mean(exp(log_blrp), na.rm = T)) %>% ungroup()
+dat <- readRDS("data-generated/b-status-dat.rds") %>%
+  filter(year >= 2000) %>%
+  rename(dfo_area = region) %>%
+  select(-species) %>%
+  group_by(stock) %>%
+  mutate(mean_blrp = mean(exp(log_blrp), na.rm = T)) %>%
+  ungroup()
 dat <- left_join(dat, stock_df)
-d <- left_join(all_indices, stock_df, by = c("species", "region")) %>% rename(index = region, model_type = model) %>% mutate(model = paste(index, model_type))
+d <- left_join(all_indices, stock_df, by = c("species", "region")) %>%
+  rename(index = region, model_type = model) %>%
+  mutate(model = paste(index, model_type))
 d <- full_join(dat, d)
 
-d1 <- filter(d, !is.na(stock_clean)) %>% group_by(stock_clean, index, gear, model) %>%
-  mutate(mean_est = mean(est, na.rm = T)) %>% ungroup()
+d1 <- filter(d, !is.na(stock_clean)) %>%
+  group_by(stock_clean, index, gear, model) %>%
+  mutate(mean_est = mean(est, na.rm = T)) %>%
+  ungroup()
 
 # # change order of facets to group more like species together
 d2 <- d1 %>% arrange(desc(type), stock) # for grouping by taxa/type first
 # stock_df <- stock_df %>% arrange(stock) # for alphabetical order
 d2 <- d2 %>% mutate(stock_clean = factor(stock_clean,
-         levels = as.character(unique(stock_df$stock_clean))
+  levels = as.character(unique(stock_df$stock_clean))
 ))
 
 ggplot(d2) +
-  geom_ribbon(aes(year, ymin = q0.05_blrp/mean_blrp,  ymax = q0.95_blrp/mean_blrp,
-                  group = stock_clean), fill = "black", alpha = 0.2) +
-  geom_line(aes(year, exp(log_blrp)/mean_blrp, group = stock_clean),
-            linetype = 3, alpha = 0.4, colour = "black") +
-  geom_line(aes(year, est / mean_est, group = model, colour = gear)) + #linetype = gear,
-  geom_ribbon(aes(year, ymin = lwr / mean_est, ymax = upr / mean_est,
-                  group = model, fill = gear), alpha = 0.3) +
+  geom_ribbon(aes(year,
+    ymin = q0.05_blrp / mean_blrp, ymax = q0.95_blrp / mean_blrp,
+    group = stock_clean
+  ), fill = "black", alpha = 0.2) +
+  geom_line(aes(year, exp(log_blrp) / mean_blrp, group = stock_clean),
+    linetype = 3, alpha = 0.4, colour = "black"
+  ) +
+  geom_line(aes(year, est / mean_est, group = model, colour = gear)) + # linetype = gear,
+  geom_ribbon(aes(year,
+    ymin = lwr / mean_est, ymax = upr / mean_est,
+    group = model, fill = gear
+  ), alpha = 0.3) +
   ylab("Relative biomass estimates") +
   # scale_linetype_manual(name = "Index type", values = c(2,3,1,4)) +
   scale_color_brewer(name = "Index type", palette = "Dark2") +
   scale_fill_brewer(name = "Index type", palette = "Dark2") +
   facet_wrap(~stock_clean, ncol = 5, scales = "free_y") +
-  ggsidekick::theme_sleek() + theme(axis.text.y = element_blank(),
-                                    axis.ticks.y = element_blank())
+  ggsidekick::theme_sleek() +
+  theme(
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank()
+  )
 
 ggsave("figs/stock_vs_indices.pdf", width = 12, height = 10)
 ggsave("figs/stock_vs_indices.png", width = 12, height = 10)
@@ -56,13 +72,16 @@ ggsave("figs/stock_vs_indices.png", width = 12, height = 10)
 
 ggplot(d2, aes(year, est / mean_est), group = index, fill = gear, colour = gear) +
   geom_line(aes(linetype = gear, group = index, colour = gear)) +
-  geom_ribbon(aes(ymin = lwr / mean_est, ymax = upr / mean_est,
-                  group = index, fill = gear), alpha = 0.4) +
+  geom_ribbon(aes(
+    ymin = lwr / mean_est, ymax = upr / mean_est,
+    group = index, fill = gear
+  ), alpha = 0.4) +
   ylab("Biomass (tonnes)") +
-  scale_linetype_manual(values = c(2,3,1)) +
+  scale_linetype_manual(values = c(2, 3, 1)) +
   scale_color_brewer(palette = "Dark2") +
   scale_fill_brewer(palette = "Dark2") +
   facet_wrap(~stock_clean, scales = "free_y") +
-  ggsidekick::theme_sleek() + theme(axis.text.y = element_blank())
+  ggsidekick::theme_sleek() +
+  theme(axis.text.y = element_blank())
 
 ggsave("analysis-survey/figs/stock_indices.pdf", width = 14, height = 8)
