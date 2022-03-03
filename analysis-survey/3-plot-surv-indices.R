7b4d50213fcccdae344e7eb89f3431805ccb61a7
Sean Anderson <sean@seananderson.ca>
2022-03-02 22:39:15 -0800

    plots

--- analysis-survey/3-plot-surv-indices.R
+++ analysis-survey/3-plot-surv-indices.R
@@ -17,11 +17,11 @@ saveRDS(all_indices, file = paste0("data-generated/sopo-2021-indices.rds"))
 all_indices <- readRDS(paste0("data-generated/sopo-2021-indices.rds")) %>%
   rename(gear = type)
 dat <- readRDS("data-generated/b-status-dat.rds") %>%
-  filter(year >= 2000) %>%
+  filter(year >= 1990) %>%
   rename(dfo_area = region) %>%
   select(-species) %>%
   group_by(stock) %>%
-  mutate(mean_blrp = exp(mean(log_blrp, na.rm = TRUE)),
+  mutate(mean_blrp = exp(mean(log_blrp[year >= 2003], na.rm = TRUE)),
          ratio_ci = q0.95_blrp/q0.05_blrp ) %>%
   ungroup()
 
@@ -68,12 +68,12 @@ ggplot(all_indices, aes(year, est, group = model)) +
   theme(axis.text.y = element_blank())
 ggsave("figs/delta-vs-tweedie-best.pdf", width = 17, height = 12)
 
-slopes <- all_indices %>% group_by(species, region) %>%
+slopes <- all_indices %>% group_by(species, gear) %>%
   group_split() %>%
   purrr::map_dfr(function(.x) {
     .x <- filter(.x, year >= 2010)
     m <- lm(log(est) ~ year, data = .x)
-    data.frame(slope = coef(m)[2], species = .x$species[1], region = .x$region[1])
+    data.frame(slope = coef(m)[2], species = .x$species[1], gear = .x$gear[1])
   })
 
 all_indices %>% left_join(slopes) %>%
@@ -99,42 +99,72 @@ d <- left_join(all_indices, stock_df, by = c("species", "region")) %>%
   mutate(mean_est = exp(mean(log(est), na.rm = TRUE))) %>%
   ungroup()
 
+unique(dat$stock_clean)
 d1 <- full_join(dat, d)
+unique(d1$stock_clean)
 
 # d1 <- filter(d1, !is.na(stock_clean))
 
+# slopes2 <- left_join(slopes, select(d1, stock_clean, species, region)) %>%
+  # distinct()
+
+d1 <- left_join(d1, slopes)
+
 # # change order of facets to group more like species together
 d2 <- d1 %>% arrange(desc(type), stock) # for grouping by taxa/type first
-d2$stock_clean <- ifelse(!is.na(d2$stock_clean), d2$stock_clean, paste(d2$species, d2$index))
+d2$stock_clean <- ifelse(!is.na(d2$stock_clean), d2$stock_clean, paste(d2$species, d2$gear))
+
+unique(d2$stock_clean)
+
 # stock_df <- stock_df %>% arrange(stock) # for alphabetical order
 # d2 <- d2 %>% mutate(stock_clean = factor(stock_clean,
 #   levels = as.character(unique(stock_df$stock_clean))
 # ))
 
-# range(d2$sd_log_blrp, na.rm = T)
-d2 %>%
+d2 <- d2 %>% group_by(stock_clean) %>%
+  mutate(slope = ifelse(is.na(slope), mean(slope, na.rm = TRUE), slope)) # fill in assessment rows
+
+select(d2, species, gear, slope) %>% distinct()
+
+cols <- RColorBrewer::brewer.pal(3, name = "Dark2")
+cols <- c("#00000050", cols)
+names(cols) <- c("Assessment", "HBLL (inside)", "HBLL (outside)", "Synoptic trawl")
+
+g <- d2 %>%
+  mutate(stock_clean = gsub("([a-zA-Z]+ [a-zA-Z]+) ", "\\1\\\n", stock_clean)) %>%
+  mutate(gear = ifelse(is.na(gear), "Assessment", gear)) %>%
   ggplot() +
   geom_ribbon(aes(year,
     ymin = q0.05_blrp / mean_blrp, ymax = q0.95_blrp / mean_blrp,
     group = stock_clean
   ), fill = "black", alpha = 0.2) +
   geom_line(aes(year, exp(log_blrp) / mean_blrp, group = stock_clean),
-    linetype = 3, alpha = 0.4, colour = "black"
+    linetype = 1, alpha = 0.4, colour = "black"
   ) +
-  geom_line(aes(year, est / mean_est, group = model)) +
+  geom_line(aes(year, est / mean_est, colour = gear)) +
   geom_ribbon(aes(year,
     ymin = lwr / mean_est, ymax = upr / mean_est,
-    group = model, fill = gear
+    fill = gear
   ), alpha = 0.3) +
-  ylab("Relative biomass estimates") +
-  scale_color_brewer(name = "Index type", palette = "Dark2") +
-  scale_fill_brewer(name = "Index type", palette = "Dark2", na.value="black") +
-  facet_wrap(~stock_clean, ncol = 5, scales = "free_y") + #
+  ylab("Relative biomass or abundance estimate") +
+  # scale_color_brewer(name = "Index type", palette = "Dark2") +
+  # scale_fill_brewer(name = "Index type", palette = "Dark2") +
+  scale_colour_manual(values = cols) +
+  scale_fill_manual(values = cols) +
+  facet_wrap(vars(forcats::fct_reorder(stock_clean, -slope)),
+    ncol = 5, scales = "free_y") + #
   ggsidekick::theme_sleek() +
   theme(
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank()
-  )
+    axis.text.y = element_blank(),  axis.title.x = element_blank(),
+    axis.ticks.y = element_blank(),
+    panel.grid.major.x = element_line(colour = "grey85", linetype = 2),
+    panel.spacing.x = unit(13, "points"),
+    panel.spacing.y = unit(0, "points")
+  ) +
+  coord_cartesian(expand = FALSE) +
+  scale_y_continuous(limits = c(0, NA), expand = c(0, NA)) +
+  scale_x_continuous(expand = c(0, 0)) +
+  labs(colour = "Type", fill = "Type")
 
-ggsave("figs/stock_vs_indices.pdf", width = 14, height = 14)
-ggsave("figs/stock_vs_indices.png", width = 14, height = 14)
+ggsave("figs/stock-vs-indices.pdf", width = 10, height = 12)
+ggsave("figs/stock-vs-indices.png", width = 10, height = 12)
