e79cf11a2e516842f4b756b0ba630410678c5bc1
Sean Anderson <sean@seananderson.ca>
2022-03-03 12:22:54 -0800

    tweak fig

--- analysis-survey/3-plot-surv-indices.R
+++ analysis-survey/3-plot-surv-indices.R
@@ -18,14 +18,7 @@ all_indices <- readRDS(paste0("data-generated/sopo-2021-indices.rds")) %>%
 dat <- readRDS("data-generated/b-status-dat.rds") %>%
   filter(year >= 1960) %>%
   rename(dfo_area = region) %>%
-  select(-species) %>%
-  group_by(stock) %>%
-  mutate(
-    mean_blrp = exp(mean(log_blrp[year >= 2003], na.rm = TRUE)),
-    ratio_ci = q0.95_blrp / q0.05_blrp
-  ) %>%
-  ungroup()
-
+  select(-species)
 dat <- left_join(dat, stock_df)
 
 g <- ggplot(all_indices, aes(year, est, group = model)) +
@@ -76,7 +69,7 @@ slopes <- all_indices %>%
   group_by(species, gear) %>%
   group_split() %>%
   purrr::map_dfr(function(.x) {
-    .x <- filter(.x, year >= 2003)
+    .x <- filter(.x, year >= 2000)
     m <- lm(log(est) ~ year, data = .x)
     data.frame(slope = coef(m)[2], species = .x$species[1], gear = .x$gear[1])
   })
@@ -139,6 +132,48 @@ d2$est[d2$stock == "sablefish_BC"] <- NA # not trap survey
 d2$lwr[d2$stock == "sablefish_BC"] <- NA # not trap survey
 d2$upr[d2$stock == "sablefish_BC"] <- NA # not trap survey
 
+d3 <- group_by(d2, stock_clean) %>%
+  group_split() %>%
+  purrr::map_dfr(function(.x) {
+    if (sum(!is.na(.x$est)) == 0L) {
+      return(mutate(.x, min_geo_mean = NA, max_geo_mean = NA, mean_blrp = 1, mean_est = 1))
+    } else {
+      .x <- mutate(.x, min_surv_year = min(year[!is.na(est)]), na.rm = TRUE)
+      .x <- mutate(.x, max_surv_year = max(year[!is.na(est)]), na.rm = TRUE)
+      .x <- mutate(.x, min_assess_year = min(year[!is.na(log_blrp)]), na.rm = TRUE)
+      .x <- mutate(.x, max_assess_year = max(year[!is.na(log_blrp)]), na.rm = TRUE)
+      .x <- mutate(.x, min_geo_mean = max(min_surv_year, min_assess_year))
+      .x <- mutate(.x, max_geo_mean = min(max_surv_year, max_assess_year))
+      .x <- mutate(.x, mean_blrp = exp(mean(log_blrp[year >= min_geo_mean & year <= max_geo_mean], na.rm = TRUE)))
+      # could be multiple survey gears:
+      if (sum(!is.na(.x$log_blrp)) == 0L) { # no assessment
+        .x$min_geo_mean <- min(.x$year, na.rm = TRUE)
+        .x$max_geo_mean <- max(.x$year, na.rm = TRUE)
+      }
+      # if (unique(.x$stock_clean)[1] == "North Pacific Spiny Dogfish BC") browser()
+      .x <- group_by(.x, gear) %>%
+        mutate(mean_est = exp(mean(log(est[year >= min_geo_mean & year <= max_geo_mean]), na.rm = TRUE))) %>%
+        ungroup()
+      .x <- select(.x, -min_surv_year, -max_surv_year, -min_assess_year, -max_assess_year)
+      .x
+    }
+  })
+
+lh <- d3$slope[d3$stock_clean == "Lingcod HBLL (outside)"]
+lt <- d3$slope[d3$stock_clean == "Lingcod Synoptic trawl"]
+d3$slope[d3$stock_clean == "Lingcod HBLL (outside)"] <- mean(c(mean(lh), mean(lt)))
+d3$slope[d3$stock_clean == "Lingcod Synoptic trawl"] <- mean(c(mean(lh), mean(lt)))
+d3$stock_clean[d3$stock_clean == "Lingcod Synoptic trawl"] <- "Lingcod BC Outside"
+d3$stock_clean[d3$stock_clean == "Lingcod HBLL (outside)"] <- "Lingcod BC Outside"
+
+d3$stock_clean <- gsub("Synoptic trawl", "BC", d3$stock_clean)
+d3$stock_clean <- gsub("HBLL (outide)", "BC Outside", d3$stock_clean)
+d3$stock_clean <- gsub("VI Inside", "4B (VI Inside)", d3$stock_clean)
+# d3$stock_clean <- gsub("4B", "4B (VI Inside)", d3$stock_clean)
+d3$stock_clean <- gsub("HBLL \\(inside\\)", "4B (VI Inside)", d3$stock_clean)
+d3$stock_clean <- gsub("HBLL \\(outside\\)", "BC (Outside)", d3$stock_clean)
+# d3$stock_clean <- gsub("Dogfish BC", "DogfishBC", d3$stock_clean)
+
 make_surv_assess_plot <- function(dat) {
   dat %>%
     mutate(
@@ -177,13 +212,15 @@ make_surv_assess_plot <- function(dat) {
     scale_y_continuous(limits = c(0, NA), expand = c(0, 0)) +
     scale_x_continuous(expand = c(0, 0)) +
     labs(colour = "Type", fill = "Type")
+  # geom_vline(aes(xintercept = min_geo_mean), lty = 1) + # testing
+  # geom_vline(aes(xintercept = max_geo_mean), lty = 1) # testing
 }
 
-g <- make_surv_assess_plot(d2)
+g <- make_surv_assess_plot(d3)
 ggsave("figs/stock-vs-indices.pdf", width = 12, height = 12)
 ggsave("figs/stock-vs-indices.png", width = 12, height = 12)
 
-g <- d2 %>%
+g <- d3 %>%
   filter(year >= 2000) %>%
   make_surv_assess_plot()
 ggsave("figs/stock-vs-indices-recent.pdf", width = 10, height = 12)
