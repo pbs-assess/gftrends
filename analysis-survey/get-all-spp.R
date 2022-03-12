5987ce3802a8b423012d9fc05fde7641dff6fbb7
English <Philina.English@dfo-mpo.gc.ca>
2022-03-11 16:46:57 -0800

    to get complete spp list for 2021

--- analysis-survey/get-all-spp.R
+++ analysis-survey/get-all-spp.R
@@ -1,10 +1,8 @@
 library(dplyr)
-getwd()
-setwd(here::here("/analysis/SOPO"))
+library(gfdata)
 
-
-spp_table <- get_species() 
-saveRDS(spp_table , file = "data/allspp.rds")
+spp_table <- get_species()
+saveRDS(spp_table , file = "data-raw/allspp.rds")
 
 
 fish_table <- spp_table %>% filter(species_grouping == "fish")
@@ -12,23 +10,23 @@ species_list <- c(fish_table$species_common_name)
 species_list <- na.omit(species_list)
 species_list <- unique(species_list)
 
-invert_table <- spp_table %>% filter(species_grouping == "invertebrate")
-invert_list <- c(invert_table$species_common_name)
-invert_list <- na.omit(invert_list)
-invert_list <- unique(invert_list)
+# invert_table <- spp_table %>% filter(species_grouping == "invertebrate")
+# invert_list <- c(invert_table$species_common_name)
+# invert_list <- na.omit(invert_list)
+# invert_list <- unique(invert_list)
 
 all_spp_trawl <- list()
 for (i in species_list) {
   species <- i
-  all_spp_trawl[[i]] <- try({gfdata::get_survey_sets(species, ssid = c(1, 3))})
+  all_spp_trawl[[i]] <- try({gfdata::get_survey_sets(species, ssid = c(1,3,4,16,22,36,39,40))})
 }
 
-all_trawl <- do.call("rbind", all_spp_trawl)
-all_trawl_catch <- all_trawl %>% filter(catch_weight > 0)
-all_trawl_catch_2019 <- all_trawl_catch %>% filter(year == 2019)
+all_surv <- do.call("rbind", all_spp_trawl)
+#all_trawl_catch <- all_trawl %>% filter(catch_weight > 0)
+all_catch_2021 <- all_surv %>% filter(year == 2021)
 
-saveRDS(all_trawl_catch, file = "data/all_trawl_catch.rds")
-saveRDS(all_trawl_catch_2019, file = "data/all_trawl_catch.rds")
+saveRDS(all_surv, file = "data-raw/all_surv_catch.rds")
+saveRDS(all_catch_2021, file = "data-raw/all_trawl_catch_2021.rds")
 
 ####
 
