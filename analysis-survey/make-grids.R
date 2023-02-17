cf748d66a853cc9aca8da43b7f3b1fa38df7f924
J Dunic <jdunic@gmail.com>
2023-02-17 09:20:27 -0800

    add notes about code after talking to philina

--- analysis-survey/make-grids.R
+++ analysis-survey/make-grids.R
@@ -6,8 +6,12 @@ all_data <- readRDS(here::here("data-raw/all-survey-sets-2021.rds")) %>%
 # grid for whole coast synoptic indices
 grid_locs <- gfplot::synoptic_grid %>%
       dplyr::select(X, Y, depth, survey)
+# For now interested in only the trawl surveys
 d <- all_data %>% dplyr::filter(survey_abbrev %in% c("SYN QCS", "SYN HS", "SYN WCVI", "SYN WCHG"))
+# Need to find all the years that there was some trawl data collected in one of these
+# four surveys
 original_time <- sort(unique(d$year))
+# Building out the grid for each year a survey existed (function is now in sdmTMB)
 nd_whole_coast_index <- do.call("rbind",
   replicate(length(original_time), grid_locs, simplify = FALSE))
 nd_whole_coast_index[["year"]] <- rep(original_time, each = nrow(grid_locs))
@@ -27,14 +31,14 @@ saveRDS(nd_whole_coast_index, file = paste0(here::here("data-generated/nd_whole_
 # saveRDS(nd_all, file = paste0("analysis-survey/data/nd_hbll_outside_index.rds"))
 
 grid_locs <- readRDS(here::here("data-raw/All_HBLL_Blocks_Area_Water.rds")) %>%
-  mutate(depth = (MAX_DEPTH_ + MIN_DEPTH_)/2) %>%
-  select(survey = SS_NAME, ssid = SS_ID, latitude = LATITUDE, longitude = LONGITUDE, area = Area_Water_km2, depth)
+  mutate(depth = (MAX_DEPTH_ + MIN_DEPTH_)/2) %>%  # get midpoint depth
+  select(survey = SS_NAME, ssid = SS_ID, latitude = LATITUDE, longitude = LONGITUDE, area = Area_Water_km2, depth)  # rename to match what is in the synoptic data
 
 
 # grid for outside HBLL indices
 nd_out <- filter(grid_locs, ssid %in% c(22, 36))
 d <- all_data %>% filter(survey_abbrev %in% c("HBLL OUT S", "HBLL OUT N"))
-time <- sort(unique(d$year))
+time <- sort(unique(d$year))  # get year of the outside LL surveys
 nd_all <- do.call("rbind", replicate(length(time), nd_out, simplify = FALSE))
 nd_all[["year"]] <- rep(time, each = nrow(nd_out))
 nd_all <- sdmTMB::add_utm_columns(nd_all, c("longitude", "latitude"), utm_crs = 32609)
