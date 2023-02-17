ad9c1a4f293691a42e54f7c19d16e73c6ddcc2df
J Dunic <jdunic@gmail.com>
2023-02-17 13:12:09 -0800

    removed old code no longer needed because of: commit 316799a5c7281fa6bf9700c9a10c1034d6c6860a

--- analysis-survey/make-grids.R
+++ analysis-survey/make-grids.R
@@ -16,18 +16,7 @@ nd_whole_coast_index <- sdmTMB::replicate_df(grid_locs, "year", original_time)
 
 saveRDS(nd_whole_coast_index, file = paste0(here::here("data-generated/nd_whole_coast_index.rds")))
 
-# # grid for outside HBLL indices
-# nd_N <- gfplot::hbll_n_grid$grid %>% mutate(survey = "HBLL OUT N")
-# nd_S <- gfplot::hbll_s_grid$grid %>% mutate(survey = "HBLL OUT S")
-# nd_all1 <- bind_rows(nd_N, nd_S) %>% rename(longitude = X, latitude = Y) %>% mutate(area = 4)
-# d <- all_data$survey_sets %>% filter(survey_abbrev %in% c("HBLL OUT S", "HBLL OUT N"))
-# time <- sort(unique(d$year))
-# nd_all <- do.call("rbind", replicate(length(time), nd_all1, simplify = FALSE))
-# nd_all[["year"]] <- rep(time, each = nrow(nd_all1))
-# nd_all <- sdmTMB::add_utm_columns(nd_all, c("longitude", "latitude"), utm_crs = 32609)
-#
-# saveRDS(nd_all, file = paste0("analysis-survey/data/nd_hbll_outside_index.rds"))
-
+# Use better version of grids for HBLL? Check that they are different from gfplot::hbll_n_grid$grid?
 grid_locs <- readRDS(here::here("data-raw/All_HBLL_Blocks_Area_Water.rds")) %>%
   mutate(depth = (MAX_DEPTH_ + MIN_DEPTH_)/2) %>%  # get midpoint depth
   select(survey = SS_NAME, ssid = SS_ID, latitude = LATITUDE, longitude = LONGITUDE, area = Area_Water_km2, depth)  # rename to match what is in the synoptic data
