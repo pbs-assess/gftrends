47bde049f0abb55e186eb67f36368d5354750446
J Dunic <jdunic@gmail.com>
2023-03-03 11:51:49 -0800

    add script to make presentation figures

---
+++ analysis-survey/07-plot-presentation-figs.R
@@ -0,0 +1,95 @@
+library(dplyr)
+library(ggplot2)
+
+# Set colour for source column
+cols <- RColorBrewer::brewer.pal(3, name = "Dark2")
+cols <- c("#00000050", cols)
+
+d <- readRDS("data-generated/assessments-surveys-joined.rds")
+d <- mutate(d, gear_type = ifelse(!grepl("trawl", gear), "Longline", "Trawl"))
+d <- mutate(d, facet_label = paste(panel_title1, panel_title2, sep = "\n"))
+d <- mutate(d, source = ifelse(is.na(est), "Assessement", gear)) %>% 
+     mutate(source = factor(source, levels = c("Assessement", "HBLL (inside)", "HBLL (outside)", "Synoptic trawl")))
+
+d %>% 
+filter(type == "Roundfish") %>%
+distinct(stock_clean, gear, year, est, log_blrp, mean_blrp, facet_label, mean_est) %>% view
+
+plot_pres_trends <- function(data, ncol) {
+  ggplot(data = data) +
+  geom_ribbon(aes(year,
+    ymin = q0.05_blrp / mean_blrp, ymax = q0.95_blrp / mean_blrp,
+    group = stock_clean
+  ), fill = "black", alpha = 0.2) +
+  geom_line(aes(year, exp(log_blrp) / mean_blrp, group = stock_clean),
+    linetype = 1, alpha = 0.4, colour = "black"
+  ) +
+  geom_line(aes(year, est / mean_est, colour = source)) +
+  geom_ribbon(aes(year,
+    ymin = lwr / mean_est, ymax = upr / mean_est,
+    fill = source
+  ), alpha = 0.3) +
+  ylab("Relative biomass or abundance") +
+  ggsidekick::theme_sleek() +
+  theme(
+    axis.text.y = element_blank(), axis.title.x = element_blank(),
+    axis.ticks.y = element_blank(),
+    panel.grid.major.x = element_line(colour = "grey85", linetype = 2),
+    panel.spacing.x = unit(15, "points"),
+    panel.spacing.y = unit(3, "points")
+  ) +
+  coord_cartesian(expand = FALSE) +
+  scale_y_continuous(limits = c(0, NA), expand = c(0, 0)) +
+  scale_x_continuous(expand = c(0, 0), breaks = c(1980, 2000, 2020)) +
+  labs(colour = "Type", fill = "Type") +
+  xlab("Year") +
+  scale_color_brewer(palette = "Dark2") +
+  scale_fill_brewer(palette = "Dark2") +
+  facet_wrap(vars(forcats::fct_reorder(facet_label, -slope)), scales = "free_y", ncol = ncol) +
+  labs(fill = "Type", colour = "Type") +
+  ggsidekick::theme_sleek() +
+  theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(),
+    panel.spacing.x = unit(5, "pt"), axis.title.x = element_blank()) +
+  guides(colour = guide_legend(order = 1), fill = guide_legend(order = 1), linetype = guide_legend(order = 2))
+}
+
+# Roundfish trends
+# ----------------
+d %>%
+  filter(type == "Roundfish") %>% 
+  filter(species != "Sablefish") %>% 
+  mutate(facet_label = forcats::fct_reorder(facet_label, -slope)) %>% 
+  ggplot(data = .) +
+  geom_ribbon(aes(year,
+    ymin = q0.05_blrp / mean_blrp, ymax = q0.95_blrp / mean_blrp,
+  ), fill = "black", alpha = 0.2) +
+  geom_line(aes(year, exp(log_blrp) / mean_blrp),
+    linetype = 1, alpha = 0.4
+  ) +
+  geom_line(aes(year, est / mean_est, colour = source)) +
+  geom_ribbon(aes(year,
+    ymin = lwr / mean_est, ymax = upr / mean_est,
+    fill = source
+  ), alpha = 0.3) +
+  ylab("Relative biomass or abundance") +
+  ggsidekick::theme_sleek() +
+  theme(
+    axis.text.y = element_blank(), axis.title.x = element_blank(),
+    axis.ticks.y = element_blank(),
+    panel.grid.major.x = element_line(colour = "grey85", linetype = 2),
+    panel.spacing.x = unit(15, "points"),
+    panel.spacing.y = unit(3, "points")
+  ) +
+  coord_cartesian(expand = FALSE) +
+  scale_y_continuous(limits = c(0, NA), expand = c(0, 0)) +
+  scale_x_continuous(expand = c(0, 0), breaks = c(1980, 2000, 2020)) +
+  labs(colour = "Type", fill = "Type") +
+  xlab("Year") +
+  scale_colour_manual(values = cols, drop = FALSE) + 
+  scale_fill_manual(values = cols, drop = FALSE) + 
+  facet_wrap(vars(facet_label), scales = "free_y", ncol = 2) +
+  labs(fill = "Type", colour = "Type") +
+  ggsidekick::theme_sleek() +
+  theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(),
+    panel.spacing.x = unit(5, "pt"), axis.title.x = element_blank()) +
+  guides(colour = guide_legend(order = 1), fill = guide_legend(order = 1), linetype = guide_legend(order = 2))
